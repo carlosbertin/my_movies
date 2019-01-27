8b26368675368dbabe98bfdc1220b30d45945283
Carlos Bertin <carlosbertin@id.uff.br>
2019-01-27 00:11:16 -0200

    finalizando slides aula02.pdf. Aprendi a usar o seed, buscas de registros com comandos via rails c e relacionamentos automáticos entre as classes pai e subclasses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# movies = [
+#     {title: 'Alladin', release_date: '25-Nov-1992'},
+#     {title: 'When Harry Met Sally', release_date: '21-Jul-1989'},
+#     {title: 'The Help', release_date: '10-Aug-2011'},
+#     {title: 'Raiders of the Lost Ark', release_date: '12-Jun-1981'}
+# ]
+
+# movies.each do |movie|
+#     Movie.create!(movie)
+# end
+
+actors = [
+    {name: 'Margot Robbie', gender: 'female'},
+    {name: 'Judit Ferrer', gender: 'female'},
+    {name: 'Zilda Camarão', gender: 'female'},
+    {name: 'Rachel Trizz', gender: 'female'},
+    {name: 'Harold Meers', gender: 'male'},
+    {name: 'Silvia Petler', gender: 'female'},
+    {name: 'Robbie Williams', gender: 'female'},
+    {name: 'David Hornet', gender: 'male'},
+]
+
+directors = [
+    {name: 'Mel Gibson', gender: 'male'},
+    {name: 'Robson Trur', gender: 'male'},
+    {name: 'Liconln Travis', gender: 'male'},
+    {name: 'Regina Bealer', gender: 'female'},
+    {name: 'Javier Rodrigues Martin', gender: 'male'},
+    {name: 'Michele Seans', gender: 'female'}
+]
+
+actors.each do |actor|
+    Actor.create!(actor)
+end
+
+directors.each do |director|
+    Director.create!(director)
+end
