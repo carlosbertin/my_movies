8a6521412f3abc46c656367afdc60337120fcf9a
Carlos Bertin <carlosbertin@id.uff.br>
2019-01-31 11:15:43 -0200

    ações edit e update do movies pronta

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,5 +16,38 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :new
+    end
+  end
+
+  def edit
+    id = params['id'] #pega o id na url (ver a rota em routes.rb)
+    @movie = Movie.find(id)
+  rescue ActiveRecord::RecordNotFound
+    @movie = nil
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
+  def update
+    id = params['id']
+    @movie = Movie.find(id)
+    @movie.update(movie_params)
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :edit, id: @movie.id
+    end
+  rescue ActiveRecord::RecordNotFound
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'movies' => 'movies#index'
   get 'movies/new' => 'movies#new'
   get 'movies/:id' => 'movies#show'
-  get 'movies/create'
+  post 'movies' => 'movies#create'
+  get 'movies/:id/edit' => 'movies#edit'
+  put 'movies/:id/' => 'movies#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
